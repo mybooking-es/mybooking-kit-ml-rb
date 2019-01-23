8e68c3c5020de46e60a28d1f2334e6e8fa9c9aca
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2019-01-23 14:07:24 +0100

    update libraries

--- public/js/common.js
+++ public/js/common.js
@@ -2,10 +2,7 @@ define(function(){
 
   var commonServices = {
 
-    //URL_PREFIX: 'http://localhost:3500'
-    //URL_PREFIX: 'http://mbcloud1.dev'
-    URL_PREFIX: 'https://easyride.mybooking.es'
-    //URL_PREFIX: 'https://demo-nuracar.mybooking.es'
+    URL_PREFIX: 'https://mbc-developer1.mybooking.es'
 
   };
 
